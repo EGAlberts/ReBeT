5391d1cb999069f28b20c99272a43c0ea5ceddc1
Elvin <EGAlberts@github.com>
2023-11-23 19:16:07 +0100

    separate sim

---
+++ rebet_sim/share/rebet_sim/launch/custom_world.launch.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+#
+# Copyright 2019 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Joep Tool
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import LaunchConfigurationEquals
+
+def generate_launch_description():
+
+    gui_arg = DeclareLaunchArgument(
+        'gui',
+        default_value='true',
+        description='Run with gui (true/false)')
+    
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    x_pose = LaunchConfiguration('x_pose', default='-2.0')
+    y_pose = LaunchConfiguration('y_pose', default='-0.5')
+
+    world = os.path.join(
+        get_package_share_directory('rebet'),
+        'worlds',
+        'sparse_world.world'
+    )
+
+
+    models_file_dir = os.path.join(get_package_share_directory('rebet_sim'), 'models')
+
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world, 'verbose': 'true'}.items(),
+        env={'GAZEBO_MODEL_PATH': models_file_dir + ": " + os.environ['GAZEBO_MODEL_PATH']},
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')),
+            condition=LaunchConfigurationEquals('gui', 'true')
+        
+    )
+
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    spawn_turtlebot_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose
+        }.items()
+    )
+
+    return LaunchDescription(
+        [gzserver_cmd, gzclient_cmd, robot_state_publisher_cmd, spawn_turtlebot_cmd, gui_arg]
+    )
+
+    # Add the commands to the launch description
+
+
+
+
---
+++ rebet_sim/share/rebet_sim/launch/spawn_tb3.launch.py
@@ -0,0 +1,56 @@
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import AnyLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+
+import random
+def generate_launch_description():
+    gui = LaunchConfiguration('gui')
+    gui_arg = DeclareLaunchArgument(
+        'gui',
+        default_value='true',
+        description='Run with gui (true/false)')
+
+    tb3_path = get_package_share_directory('turtlebot3_gazebo')
+    tb3_launch_path = os.path.join(
+        tb3_path, 'launch', 'turtlebot3_world.launch.py')
+    
+    this_path = get_package_share_directory('rebet_sim')
+    custom_world_launch_path = os.path.join(
+        this_path, 'launch', 'custom_world.launch.py')
+    
+
+
+    possible_poses =  [(0.88,-0.56), (0.25, 0.99), (-0.65,-1.39)] #[(1.2,-1.6)]
+    random.seed(1337)
+    x_random,y_random = random.choice(possible_poses)
+    box_x_pose = LaunchConfiguration('box_x_pose', default=str(x_random))
+    box_y_pose = LaunchConfiguration('box_y_pose', default=str(y_random))
+    custom_world =  IncludeLaunchDescription(
+        AnyLaunchDescriptionSource(custom_world_launch_path),
+        launch_arguments={
+           'gui': gui
+        }.items())
+    model_name = 'fire_hydrant_small'
+    spawn_box = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-entity', 'object_to_find_1',
+            '-database', model_name,
+            '-x', box_x_pose,
+            '-y', box_y_pose,
+            '-z', '-0.14'
+        ],
+        output='screen',
+    )
+    return LaunchDescription([
+        custom_world,
+        spawn_box,
+        gui_arg,
+    ])
